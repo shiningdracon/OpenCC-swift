715c0ad42daf57c7d2dd6235298d19414b4dd5ed
shiningdracon <shiningdracon@gmail.com>
2017-03-15 10:38:06 -0700

    Change module name to OpenCC

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenCC-swift",
+    name: "OpenCC",
     dependencies: [
         .Package(url: "https://github.com/shiningdracon/COpenCC-OSX.git", majorVersion: 1)
     ]
